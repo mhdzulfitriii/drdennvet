01a2872538230d71b9e9cba3f03600592157e63b
mhdzulfitriii <zulfitri542796@gmail.com>
2024-11-10 14:36:39 +0800

    init

--- clinic/settings.py
+++ clinic/settings.py
@@ -12,7 +12,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-=_nt5tbkd9@#%y6ntf4=ag6w!xdk8#=k+$cmheh!&ns_@6j#&r'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = ['*']
 
@@ -122,7 +122,7 @@ USE_TZ = True
 
 STATIC_URL = '/static/'
 MEDIA_URL = '/images/'
-STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static')]
+STATICFILES_DIRS = [os.path.join(BASE_DIR, 'staticfiles')]
 MEDIA_ROOT = os.path.join(BASE_DIR, 'images')
 STATIC_ROOT = '/usr/local/lsws/Example/html/drdennvert/public/static'
 

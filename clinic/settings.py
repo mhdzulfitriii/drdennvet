fd692221fa232a8d6b70af151d15e43fa7ab6bd1
mhdzulfitriii <zulfitri542796@gmail.com>
2024-11-10 14:24:19 +0800

    Update settings.py

--- clinic/settings.py
+++ clinic/settings.py
@@ -120,7 +120,7 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/5.1/howto/static-files/
 
-STATIC_URL = '/python/static/'
+STATIC_URL = '/static/'
 MEDIA_URL = '/images/'
 STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static')]
 MEDIA_ROOT = os.path.join(BASE_DIR, 'images')
